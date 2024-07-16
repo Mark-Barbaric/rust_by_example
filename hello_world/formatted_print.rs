981dbc0739d46446e96b66d0fb8ad8fd30c32fe4
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-07-16 13:29:42 +0100

    primitives - updated examples to use modules.

--- hello_world/formatted_print.rs
+++ hello_world/formatted_print.rs
@@ -1,16 +1,16 @@
-fn print_test() {
+pub fn print_test() {
     let x = 68200;
     println!("Base 10: {}", x);
     println!("Base 2 (binary): {:b}", x);
 }
 
-fn print_with_padding() {
+pub fn print_with_padding() {
     let x = 1;
     println!("X with left padding: {number:0>5}", number=x);
     println!("X with right padding: {number:0<5}", number=x);
 }
 
-fn print_arguments() {
+pub fn print_arguments() {
     let first_name = "Mark";
     let second_name = "Barbaric";
     println!("My name is {second_name}, {first_name} {second_name}", first_name=first_name, second_name=second_name);
@@ -18,10 +18,4 @@ fn print_arguments() {
 
 #[allow(dead_code)]
 fn dead_method() {
-}
-
-fn main() {
-    print_test();
-    print_with_padding();
-    print_arguments();
 }
--- hello_world/hello.rs
+++ hello_world/hello.rs
@@ -1,6 +1,10 @@
-fn main() {
+mod formatted_print;
 
+fn main() {
     let x = 5 + 95;
     println!("Is `x` 10 or 100? x = {}", x);
     println!("Hello world");
+    formatted_print::print_test();
+    formatted_print::print_with_padding();
+    formatted_print::print_arguments();
 }
